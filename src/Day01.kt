95503e66e337f261406a20d66f5ecb6d1c6c6989
undermark5 <undermark5@undermark5.com>
2025-12-04 19:14:54 -0600

    Day 1 complete Gold Star

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,65 @@
 fun main() {
+    val dialStart = 50
+
+
     fun part1(input: List<String>): Int {
-        return input.size
+        var curPoint = dialStart
+        var curCount = 0
+        input.forEach {
+            val magnitude = it.drop(1).toInt()
+            val direction = it.first()
+            if (direction == 'R') {
+                curPoint += magnitude
+                curPoint %= 100
+            } else {
+                curPoint += 100 - magnitude
+                curPoint %= 100
+            }
+            if (curPoint == 0) {
+                curCount++
+            }
+        }
+        return curCount
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        var curPoint = dialStart
+        var curCount = 0L
+        input.forEach {
+            val magnitude = it.drop(1).toInt().let {
+                if (it >= 100) {
+                    curCount += it / 100
+                    it % 100
+                } else {
+                    it
+                }
+            }
+            val direction = it.first()
+            if (direction == 'R') {
+                val prevPoint = curPoint
+                curPoint += magnitude
+                curPoint %= 100
+                if ((curPoint <= prevPoint && prevPoint != 0) || curPoint == 0) {
+                    curCount++
+                }
+            } else {
+                val prevPoint = curPoint
+                curPoint += 100 - magnitude
+                curPoint %= 100
+                if ((curPoint >= prevPoint && prevPoint != 0) || curPoint == 0) {
+                    curCount++
+                }
+            }
+        }
+        return curCount
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 3)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
-    part1(input).println()
+//    part1(input).println()
+    check(part2(testInput) == 6L)
     part2(input).println()
 }
